80ac4ef1fd64c72e43e0fee3ca00fa896f803c52
Sash Man <sashman90@gmail.com>
2013-02-21 10:58:42 +0000

    minor code move about

--- rooms_server.py
+++ rooms_server.py
@@ -12,37 +12,44 @@ NORTH = 'N'
 EAST = 'E'
 SOUTH = 'S'
 
+def get_random_room_dimentions():
+	sizeh = random.randint(3, 6) * TILE_SIZE
+	sizew = random.randint(3, 6) * TILE_SIZE
+	x = random.randint(sizew, width-(sizew))
+	y = random.randint(sizeh, height-(sizeh))
+	return x,y,sizew,sizeh
+
 
 number_of_rooms = 50
-	rooms = []
+rooms = []
 
-	# for n in range(number_of_rooms):
-		
+# for n in range(number_of_rooms):
+	
+x,y,sizew,sizeh = get_random_room_dimentions()
+
+x = int(width/2)
+y = int(height/2)
+
+room = Room((x,y), (sizew,sizeh))	
+rooms.append(room)	
+
+
+
+cs = []
+
+for n in range(number_of_rooms):
+
+	
+	c = random.choice(rooms).get_wall_tile()
+
 	x,y,sizew,sizeh = get_random_room_dimentions()
 
-	x = int(width/2)
-	y = int(height/2)
-
-	room = Room((x,y), (sizew,sizeh))	
-	rooms.append(room)	
-
-
-
-	cs = []
-
-	for n in range(number_of_rooms):
-
-		
-		c = random.choice(rooms).get_wall_tile()
-
-		x,y,sizew,sizeh = get_random_room_dimentions()
-
-		new_room = Room((0,0), (sizew,sizeh), tile = c)	
-		
-		if(not new_room.collides(rooms)):
-			rooms.append(new_room)
-			c.type = 'door'
-			cs.append(c)
+	new_room = Room((0,0), (sizew,sizeh), tile = c)	
+	
+	if(not new_room.collides(rooms)):
+		rooms.append(new_room)
+		c.type = 'door'
+		cs.append(c)
 
 
-	floor_tile = random.choice([tile for tile in room.tiles if tile.type == 'floor'])
+floor_tile = random.choice([tile for tile in room.tiles if tile.type == 'floor'])
